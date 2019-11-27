6c1d4a3e29c89add4d89d2b31bcd56adbf7ce8e2
Garion Herman <garion@silverstripe.com>
2019-11-27 16:24:42 +1300

    Move vendor-expose to end of process, fix indentation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,34 +4,38 @@ set -e
 source /funcs.sh
 
 if [ -d ".git" ]; then
-    SHA=$(git rev-parse HEAD)
+	SHA=$(git rev-parse HEAD)
 else
-    echo "Unable to determine SHA, failing."
-    exit 1
+	echo "Unable to determine SHA, failing."
+	exit 1
 fi
 
 if [[ "z${IDENT_KEY}" == "z" ]]; then
-    echo "No deploy key set"
+	echo "No deploy key set"
 else
-    mkdir -p ~/.ssh
-    echo "${IDENT_KEY}" > ~/.ssh/id_rsa
-    chmod 0600 ~/.ssh/id_rsa
-    FINGER_PRINT=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa | awk '{ print $2 }' | cut -c 5-)
-    echo "Using deploy key ${FINGER_PRINT}"
+	mkdir -p ~/.ssh
+	echo "${IDENT_KEY}" > ~/.ssh/id_rsa
+	chmod 0600 ~/.ssh/id_rsa
+	FINGER_PRINT=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa | awk '{ print $2 }' | cut -c 5-)
+	echo "Using deploy key ${FINGER_PRINT}"
 fi
 
 composer_install
 
 # Run NPM/Yarn build script if the cloud-build command is defined
 if [[ -f package.json && "`cat package.json | jq '.scripts["cloud-build"]?'`" != "null" ]]; then
-    nvm_switch
+	nvm_switch
 
-    node_build
+	node_build
 fi
 
 # Run Composer build script if the cloud-build command is defined
 if [[ -f composer.json && "`cat composer.json | jq '.scripts["cloud-build"]?'`" != "null" ]]; then
 	composer_build
+fi
+
+if [[ -f vendor/silverstripe/vendor-plugin/composer.json ]]; then
+	composer_vendor_expose
 fi
 
 package_source ${SHA}
